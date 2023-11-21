a8384e0a4051897f1d454ffd171355df972403bf
Bob156590 <abad04002@utb.vaxjo.se>
2023-11-21 17:25:24 +0100

    Done with the Beta version. I think

--- movement/Assets/BattleSystem/Player_FightSkript.cs
+++ movement/Assets/BattleSystem/Player_FightSkript.cs
@@ -5,12 +5,15 @@ public class Player_FightSkript : MonoBehaviour
 {
     public float playerHP;
     public float distance;
+    HealthBar healthBar;
     GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
         playerHP = 100;
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        healthBar = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<HealthBar>();
+        healthBar.MaxHealth(playerHP);
     }
 
     // Update is called once per frame
@@ -23,6 +26,7 @@ public class Player_FightSkript : MonoBehaviour
 
     public void Takedamage(float dmg){
         playerHP -= dmg;
+        healthBar.SetHealth(playerHP);
         gameManager.UpdateGameState(GameState.PlayerTurn);
     }
 }
